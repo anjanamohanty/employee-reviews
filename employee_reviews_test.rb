b3c5b006d7ba393a55f60ffb2bd2e9f7872c925b
anjanamohanty <anjana.j.mohanty@gmail.com>
2016-02-11 12:41:36 -0500

    Add Employee and Department classes

--- employee_reviews_test.rb
+++ employee_reviews_test.rb
@@ -6,7 +6,33 @@ require './department.rb'
 
 class EmployeeReviewsTest < Minitest::Test
 
+  def test_classes_exist
+    assert Employee
+    assert Department
+  end
 
+  # Create a new department (given its name).
+  
+  # Create a new employee (given his/her name, email address, phone number, and salary).
 
+  # Add an employee to a department.
+
+  # Get an employee's name.
+
+  # Get an employee's salary.
+
+  # Get a department's name.
+
+  # Get a total salary for all employees in a department.
+
+  # Add some employee review text (a paragraph or two) to an employee.
+
+  # Mark whether an employee is performing satisfactorily or not satisfactorily.
+
+  # Give a raise to an individual. You decide if this makes sense in dollars or in percent.
+
+  # Give raises to a department's employees. You must pass this method a total dollar amount,
+  # and it must distribute the raise amounts reasonably to the department's employees.
+  # Only employees who are performing satisfactorily should get raises.
 
 end
